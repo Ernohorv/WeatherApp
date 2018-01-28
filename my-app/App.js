ee21ac7bd80b8837aa9f0feeb2b0ec6851556085
Ernohorv <32036011+Ernohorv@users.noreply.github.com>
2018-01-28 16:55:36 +0100

    Api ok

--- my-app/App.js
+++ my-app/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import axios from 'axios';
 
-export default class App extends React.Component {
+const API_KEY = "2da3b95e31415153dedfffbce6d34349";
+
+export default class weatherApp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      zipcode: 2351,
+      days: [],
+    }
+  }
+
+  getForecast(zipcode){
+
+    const url ="http://api.openweathermap.org/data/2.5/weather?q=Budapest,mode=json&units=metric&appid="+API_KEY;
+  
+    axios.get(url).then((response) => {
+        if(response.status==200){
+          console.log(response.data);
+        }
+    });
+  }
   render() {
+    if( this.state.days.length <= 0 ){
+      this.getForecast(this.state.zipcode);
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+     <View style={styles.container}>
+       <StatusBar hidden/>
+       </View>
     );
   }
 }
