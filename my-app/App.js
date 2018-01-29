82242a1394a060aaa044e3c70b8d87def95328d7
Ernohorv <32036011+Ernohorv@users.noreply.github.com>
2018-01-29 14:40:35 +0100

    Wind, pressure, clouds

--- my-app/App.js
+++ my-app/App.js
@@ -2,42 +2,57 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar, ImageBackground } from 'react-native';
 import axios from 'axios';
 import  styles from './style';
-const API_KEY = "f1779fbd5b8a64760a48bcc2a9137295";
+import fetchWeather from './api'
+
+const API_KEY = "6aa351323e5575ba4f0dc69f03dbd235";
 
 export default class weatherApp extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      days : [],
       temp : 0,
-      humidity: 0
-      
+      humidity: 0,
+      city: 'Budapest',
+      pressure: 0,
+      windSpeed: '',
+      clouds: '',
+      icon: '',
+      days: []
     }
   }
 
-  getForecast(zipcode){
+  getWeather(){
 
-    const url ="http://api.openweathermap.org/data/2.5/weather?q=Budapest,mode=json&units=metric&appid="+API_KEY;
+    //const url ="http://api.openweathermap.org/data/2.5/weather?q=Budapest,mode=json&units=metric&appid="+API_KEY;
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=Budapest,mode=json&units=metric&appid=" + API_KEY;
   
     axios.get(url).then((response) => {
-        if(response.status==200){
-          //console.log(response.data);
-          var weather = [];
-          weather['temp'] = JSON.stringify(response.data.main.temp);
-          weather['humidity'] = JSON.stringify(response.data.main.humidity);
-          console.log(weather['temp']);
-          //alert(weather['temp']);
-         this.setState({temp: weather['temp']});
-         this.setState({humidity: weather['humidity']});
-        }
+
+      if(response.status == 200){
+     // console.log(JSON.stringify(response.data));
+      
+     
+      var weather = [];
+      weather['temp'] = JSON.stringify(response.data.main.temp);
+
+        this.setState({
+          temp: weather['temp'],
+          humidity: JSON.stringify(response.data.main.humidity),
+          pressure: JSON.stringify(response.data.main.pressure),
+          windSpeed: JSON.stringify(response.data.wind.speed),
+          clouds: JSON.stringify(response.data.clouds.all),
+          icon: JSON.stringify(response.data.weather.icon)
+        });
+      }
     });
+     
+          
   }
   render() {
-    if(this.state.days.length <= 0){
-      this.getForecast(this.state.days);
-    }
-
+    if(this.state.days.length <=0)
+      this.getWeather(this.state.city);
+    
     return (
       <ImageBackground 
         source={require('./maxresdefault.jpg')}
@@ -49,12 +64,27 @@ export default class weatherApp extends React.Component {
 
      <View style={styles.viewStyle}>  
        <Text style={styles.viewText}>Temperature: </Text>
-       <Text style={styles.viewText}>{this.state.temp}</Text>
+       <Text style={styles.viewText}>{this.state.temp} °</Text>
       </View>
 
       <View style={styles.viewStyle}>
       <Text style={styles.viewText}>Humidity: </Text>
       <Text style={styles.viewText}>{this.state.humidity}</Text> 
+      </View>
+
+      <View style={styles.viewStyle}>
+      <Text style={styles.viewText}>Pressure: </Text>
+      <Text style={styles.viewText}>{this.state.pressure}</Text> 
+      </View>
+
+      <View style={styles.viewStyle}>
+      <Text style={styles.viewText}>Wind speed: </Text>
+      <Text style={styles.viewText}>{this.state.windSpeed} km/h</Text> 
+      </View>
+
+      <View style={styles.viewStyle}>
+      <Text style={styles.viewText}>Clouds: </Text>
+      <Text style={styles.viewText}>{this.state.clouds} %</Text> 
       </View>
 
        </ImageBackground>
--- my-app/style.js
+++ my-app/style.js
@@ -5,7 +5,7 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         width: '100%',
-        height: '100%'
+        height: '100%',
     },
 
     header: {
