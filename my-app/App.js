f4145401d3cda703db1b4f528c611f62a0ec1df2
Ernohorv <32036011+Ernohorv@users.noreply.github.com>
2018-01-29 10:28:45 +0100

    Set state

--- my-app/App.js
+++ my-app/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import axios from 'axios';
-
-const API_KEY = "2da3b95e31415153dedfffbce6d34349";
+import  styles from './style';
+const API_KEY = "4aa93ae05f4ec9a99aaea662e565bd86";
 
 export default class weatherApp extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      zipcode: 2351,
-      days: [],
+      temp : 0,
+      days : [],
+      humidity: 0
+      
     }
   }
 
@@ -20,27 +22,31 @@ export default class weatherApp extends React.Component {
   
     axios.get(url).then((response) => {
         if(response.status==200){
-          console.log(response.data);
+          //console.log(response.data);
+          var weather = [];
+          weather['temp'] = JSON.stringify(response.data.main.temp);
+          weather['humidity'] = JSON.stringify(response.data.main.humidity);
+          console.log(weather['temp']);
+          //alert(weather['temp']);
+         this.setState({temp: weather['temp']});
+         this.setState({humidity: weather['humidity']});
         }
     });
   }
   render() {
-    if( this.state.days.length <= 0 ){
-      this.getForecast(this.state.zipcode);
+    if(this.state.days.length <= 0){
+      this.getForecast(this.state.days);
     }
+    
+    weather = [];
+    //weather['temp'] = this.state.wet;
+    //weather['temp'] = JSON.stringify(response.data.main.temp);
     return (
      <View style={styles.container}>
        <StatusBar hidden/>
+       <Text>{this.state.temp}</Text>
+       <Text>{this.state.humidity}</Text>
        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
