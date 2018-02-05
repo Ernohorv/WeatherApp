91f32a4e1b102a1e51dc167049ebc67ebf116369
Tamás <1234567zzz@gmail.com>
2018-02-05 15:52:01 +0100

    Added basic navigation
    Added basic navigation using react-navigation
    See also: #2

--- my-app/App.js
+++ my-app/App.js
@@ -1,82 +1,126 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar, ImageBackground, Button, TextInput } from 'react-native';
-import  styles from './style';
+import styles from './style';
+import { StackNavigator, } from 'react-navigation';
 
 const API_KEY = "e779919406888af3f3e84022f6154886";
 
-export default class weatherApp extends React.Component {
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Details',
+  };
 
   constructor(props) {
     super(props);
     this.state = {
-      temp : 0,
+      temp: 0,
       humidity: 0,
       city: '',
       pressure: 0,
       clouds: 0,
       icon: '',
     }
-    this.getWeather = this.getWeather.bind(this); 
+    this.getWeather = this.getWeather.bind(this);
   }
-  
-  getWeather(){
 
+  getWeather() {
     let url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.state.city + "&units=metric&appid=" + API_KEY;
 
     return fetch(url)
-      .then((response) => response.json()) 
-        .then((res) => {
-          this.setState({
-            temp: res.main.temp,
-            humidity: res.main.humidity,
-            pressure: res.main.pressure,
-            clouds: res.clouds.all,
+      .then((response) => response.json())
+      .then((res) => {
+        this.setState({
+          temp: res.main.temp,
+          humidity: res.main.humidity,
+          pressure: res.main.pressure,
+          clouds: res.clouds.all,
         })
       }
-    );
-   }
+      );
+  }
 
   render() {
-
     return (
-      <ImageBackground 
+      <ImageBackground
         source={require('./maxresdefault.jpg')}
         style={styles.container}>
-    
-     <View style={styles.header}>
-        <TextInput style={styles.headerText}
-        onChangeText={(city) => this.setState({city})}/>
-     </View>
 
-     <View style={styles.viewStyle}>  
-       <Text style={styles.viewText}>Temperature: </Text>
-       <Text style={styles.viewText}>{this.state.temp} °</Text>
-      </View>
+        <View style={styles.header}>
+          <TextInput style={styles.headerText}
+            onChangeText={(city) => this.setState({ city })} />
+        </View>
 
-      <View style={styles.viewStyle}>
-        <Text style={styles.viewText}>Humidity: </Text>
-        <Text style={styles.viewText}>{this.state.humidity} </Text>
-      </View>
+        <View style={styles.viewStyle}>
+          <Text style={styles.viewText}>Temperature: </Text>
+          <Text style={styles.viewText}>{this.state.temp} °</Text>
+        </View>
 
-      <View style={styles.viewStyle}>
-        <Text style={styles.viewText}>Pressure: </Text>
-        <Text style={styles.viewText}>{this.state.pressure} Pa</Text>
-      </View>
+        <View style={styles.viewStyle}>
+          <Text style={styles.viewText}>Humidity: </Text>
+          <Text style={styles.viewText}>{this.state.humidity} </Text>
+        </View>
 
-      <View style={styles.viewStyle}>
-        <Text style={styles.viewText}>Clouds: </Text>
-        <Text style={styles.viewText}>{this.state.clouds} </Text>
-      </View>
-      
-      <View style={styles.viewStyle}>
-        <Button 
-          style={styles.buttonStyle} 
-          title='Refresh'
-          onPress={this.getWeather}>
+        <View style={styles.viewStyle}>
+          <Text style={styles.viewText}>Pressure: </Text>
+          <Text style={styles.viewText}>{this.state.pressure} Pa</Text>
+        </View>
+
+        <View style={styles.viewStyle}>
+          <Text style={styles.viewText}>Clouds: </Text>
+          <Text style={styles.viewText}>{this.state.clouds} </Text>
+        </View>
+
+        <View style={styles.viewStyle}>
+          <Button
+            style={styles.buttonStyle}
+            title='Refresh'
+            onPress={this.getWeather}>
           </Button>
-      </View>
+        </View>
 
-       </ImageBackground>
+
+
+      </ImageBackground>
+    );
+  }
+}
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Details: {
+      screen: DetailsScreen,
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class weatherApp extends React.Component {
+  render() {
+    return (
+      <RootStack />
     );
   }
 }
